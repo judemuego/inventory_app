264fe832b5f8cd907078e5962877be17b257d72a
jude <judemuego@gmail.com>
2019-09-11 15:32:58 +0800

    inventory

--- resources/views/pages/inventory.blade.php
+++ resources/views/pages/inventory.blade.php
@@ -6,7 +6,7 @@
                     <h4 class="card-title">Inventory</h4>
                     <p class="category"></p>
                     <div class="card-options">
-                        <a href=""><i class="material-icons">add_circle_outline</i></a>
+                        <a href="" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="material-icons">add_circle_outline</i></a>
                     </div>
                             
                 </div>
@@ -15,24 +15,27 @@
                                 <thead>
                                     <tr>
                                         <th class="text-center">#</th>
-                                        <th>Name</th>
-                                        <th>Job Position</th>
-                                        <th>Since</th>
-                                        <th class="text-right">Salary</th>
+                                        <th>Item No.</th>
+                                        <th>Description</th>
+                                        <th>Alternate No.</th>
+                                        <th>Manufacturer</th>
+                                        <th>Model</th>
+                                        <th>Category</th>
+                                        
                                         <th class="text-right">Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <td class="text-center">1</td>
-                                        <td>Andrew Mike</td>
-                                        <td>Develop</td>
-                                        <td>2013</td>
-                                        <td class="text-right">&euro; 99,225</td>
+                                        <td class="text-center">2</td>
+                                        <td>ITM0002</td>
+                                        <td>Item Description 2</td>
+                                        <td>Alternate No. 000002</td>
+                                        <td>Manufacturer Two</td>
+                                        <td>Model - 2202 Sample</td>
+                                        <td>Category 2</td>
+                                        
                                         <td class="td-actions text-right">
-                                            <button type="button" rel="tooltip" class="btn btn-info">
-                                                <i class="material-icons">person</i>
-                                            </button>
                                             <button type="button" rel="tooltip" class="btn btn-success">
                                                 <i class="material-icons">edit</i>
                                             </button>
@@ -42,44 +45,59 @@
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td class="text-center">2</td>
-                                        <td>John Doe</td>
-                                        <td>Design</td>
-                                        <td>2012</td>
-                                        <td class="text-right">&euro; 89,241</td>
-                                        <td class="td-actions text-right">
-                                            <button type="button" rel="tooltip" class="btn btn-info btn-round">
-                                                <i class="material-icons">person</i>
-                                            </button>
-                                            <button type="button" rel="tooltip" class="btn btn-success btn-round">
-                                                <i class="material-icons">edit</i>
-                                            </button>
-                                            <button type="button" rel="tooltip" class="btn btn-danger btn-round">
-                                                <i class="material-icons">close</i>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                    <tr>
                                         <td class="text-center">3</td>
-                                        <td>Alex Mike</td>
-                                        <td>Design</td>
-                                        <td>2010</td>
-                                        <td class="text-right">&euro; 92,144</td>
+                                        <td>ITM0003</td>
+                                        <td>Item Description 3</td>
+                                        <td>Alternate No. 000003</td>
+                                        <td>Manufacturer One</td>
+                                        <td>Model - 2203 Sample</td>
+                                        <td>Category 3</td>
+                                        
                                         <td class="td-actions text-right">
-                                            <button type="button" rel="tooltip" class="btn btn-info btn-simple">
-                                                <i class="material-icons">person</i>
-                                            </button>
-                                            <button type="button" rel="tooltip" class="btn btn-success btn-simple">
+                                            <button type="button" rel="tooltip" class="btn btn-success">
                                                 <i class="material-icons">edit</i>
                                             </button>
-                                            <button type="button" rel="tooltip" class="btn btn-danger btn-simple">
+                                            <button type="button" rel="tooltip" class="btn btn-danger">
                                                 <i class="material-icons">close</i>
                                             </button>
                                         </td>
                                     </tr>
+                                    
                                 </tbody>
                             </table>
                 </div>
             </div>
 </div>
+
+
+<!-- Large modal -->
+
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+    <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Add Vehicles</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+        <form>
+            <div class="form-group">
+              <input type="text" class="form-control" id="inputUserNo" placeholder="Vehicle No.">
+            </div>
+            <div class="form-group">
+                <input type="email" class="form-control" id="inputEmail" placeholder="Plate No.">
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" id="inputDescription" placeholder="Description">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" id="inputPassword" placeholder="Vehicle Type">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+    </div>
+  </div>
+</div>
+
 @endsection
